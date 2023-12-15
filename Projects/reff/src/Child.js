40aa13021fdfa095afb34ca57ff6405bef92503e
prasad-bigdp <bigdp585@gmail.com>
2023-12-15 10:52:02 +0530

    update

--- Projects/reff/src/Child.js
+++ Projects/reff/src/Child.js
@@ -1,22 +1,24 @@
-import React,{useImperativeHandle} from 'react'
+import React, { useImperativeHandle } from 'react'
+import {useState} from 'react'
 
 const Child = React.forwardRef(function MyComponent (props, ref)
 {
+	cost[(value, setValue)] = useState(0)
     useImperativeHandle(
 			ref,
 			() => {
 				return {
 					focus() {
 						ref.current.focus()
-					},
+					}
 					
 				}
 			},
-			[],
+			[value],
 		)
   return (
     <div>
-      <input type="text" ref={ref} />
+      <input type="text" ref={ref} onChange={()=>setValue(value+1)}/>
     </div>
   )
 })
